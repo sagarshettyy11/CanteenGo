c189e7114aa6551d273d4825cc28e8ce31b76a5c
Sagar Shetty <sagarshettyy11@gmail.com>
2026-03-11 11:32:57 +0530

    Resolved merge conflict in main.dart

--- canteengo/lib/main.dart
+++ canteengo/lib/main.dart
@@ -1,11 +1,10 @@
-// ignore: unused_import
-import 'package:canteengo/pages/cart_page.dart';
-import 'package:canteengo/pages/order_history_page.dart';
 import 'package:flutter/material.dart';
+import 'package:snackly/pages/cart_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
